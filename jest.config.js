1befb921fbf8a54005ea7ee2e6ad15d7d47dfc56
joe <jsh901220@gmail.com>
2020-02-07 03:46:33 +0900

    [dev: jest] - [fix] jest clean up is no needed cause version

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
       diagnostics: true
     }
   },
-
+  setupFilesAfterEnv: ["./src/setupTests.ts"],
   coverageReporters: ["json", "lcov", "text", "text-summary"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
