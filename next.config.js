aa2f03b3496b7e342978f8e54561aa3291dff7e4
joe <jsh901220@gmail.com>
2020-05-29 01:19:05 +0900

    [bug: css] - [fix] css module일 때 css 미적용

--- next.config.js
+++ next.config.js
@@ -1,19 +1,30 @@
 const path = require("path")
 const withSass = require("@zeit/next-sass")
+const withCSS = require("@zeit/next-css")
 
-module.exports = withSass({
-  webpack: (config, options) => {
-    config.resolve.alias["src"] = path.join(__dirname, "src")
-    config.resolve.alias["pages"] = path.join(__dirname, "pages")
-
-    return config
-  },
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]"
-  },
-  sassOptions: {
-    includePaths: [path.resolve(__dirname, "src"), path.resolve(__dirname, "pages")]
-  }
-})
+module.exports = withCSS(
+  withSass({
+    webpack: (config, options) => {
+      config.resolve.alias["src"] = path.join(__dirname, "src")
+      config.resolve.alias["pages"] = path.join(__dirname, "pages")
+      config.module.rules.forEach((rule) => {
+        if (String(rule.test) === String(/\.css$/)) {
+          rule.use.forEach((u) => {
+            if (u.options) {
+              u.options.modules = false
+            }
+          })
+        }
+      })
+      return config
+    },
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
+    },
+    sassOptions: {
+      includePaths: [path.resolve(__dirname, "src"), path.resolve(__dirname, "pages")],
+    },
+  })
+)
