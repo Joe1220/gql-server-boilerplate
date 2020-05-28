20ae18c280213b65a17f803a1be88e5bf7dc031f
joe <jsh901220@gmail.com>
2020-05-29 01:58:02 +0900

    [dev: apollo] - [add] apollo console log setting

--- src/apollo/index.tsx
+++ src/apollo/index.tsx
@@ -6,4 +6,8 @@ import { baseUrl } from "src/shared/config"
 export default new ApolloClient({
   uri: baseUrl,
   fetch,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors)
+    console.log("networkError", networkError)
+  },
 })
