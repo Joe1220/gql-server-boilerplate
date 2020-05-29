08153ed78b74757e4b920bfcf52136b92039a0fb
joe <jsh901220@gmail.com>
2020-05-29 16:19:33 +0900

    [dev: style]  - [fix] loading container style  수정

--- pages/_app/index.tsx
+++ pages/_app/index.tsx
@@ -10,6 +10,8 @@ import initialStore, { PersistedStore } from "src/store"
 import "src/styles/index.scss"
 import LoadingSpinner from "src/components/atoms/LoadingSpinner/index"
 import apolloClient from "src/apollo"
+import ToastContainer from "src/toastify"
+import styles from "./_app.scss"
 
 export interface IProps {
   store: PersistedStore
@@ -21,8 +23,16 @@ class MyApp extends App<IProps> {
     return (
       <Provider store={store}>
         <ApolloProvider client={apolloClient}>
-          <PersistGate loading={<LoadingSpinner />} persistor={store.__persistor}>
+          <PersistGate
+            loading={
+              <div className={styles.app__loading__container}>
+                <LoadingSpinner />
+              </div>
+            }
+            persistor={store.__persistor}
+          >
             <Component {...pageProps} />
+            <ToastContainer />
             <Head>
               <title>FClock</title>
             </Head>
